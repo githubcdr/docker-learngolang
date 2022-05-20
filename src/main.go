ead707ed461eabece88662fbf269422617248135
cDR <me@codar.nl>
2022-05-20 22:43:56 +0200

    chore: go mod

--- src/main.go
+++ src/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintln(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintln(w, "Header", r.Header.Get("User-Agent"))
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
